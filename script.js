6ab52bf345459662a81ef705c7c79acd6d5dd7c1
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2023-07-07 11:16:06 +0530

    updated navbar

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let searchBtn =document.querySelector('#search-btn');
+// JavaScript code (script.js)
+let searchBtn = document.querySelector('#search-btn');
 let searchForm = document.querySelector('.header .search-form');
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
-searchBtn.onclick = () =>{
+searchBtn.onclick = () => {
     searchBtn.classList.toggle('fa-times');
     searchForm.classList.toggle('active');
-}
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
 
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+};
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
